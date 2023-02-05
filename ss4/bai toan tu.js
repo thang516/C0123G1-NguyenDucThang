aa83d40a459adc02f98c88e2b724efea9dbcb9c3
thang516 <nguyenthangfa2001@gmail.com>
2023-02-05 23:01:25 +0700

    bai 4 va doi ten file

---
+++ ss4/bai toan tu.js
@@ -0,0 +1,31 @@
+inputdiemly = prompt("enter Điểm lý");
+inputdiemhoa = prompt("enter Điểm hóa");
+inputdiemsinh = prompt("Enter Điểm Sinh");
+let ly = parseInt(inputdiemly);
+let hoa = parseInt(inputdiemhoa);
+let sinh = parseInt(inputdiemsinh);
+let diemtb =  alert((ly + hoa + sinh)/3);
+let điểmtổng = alert( ly + hoa + sinh);
+
+
+input0c = prompt("Enter 0c");
+let do0c = parseInt(input0c);
+alert(( 160 + do0c * 9) / 5 );
+
+
+
+
+inputbankinh = prompt("enter bán kính");
+let property_value = Math.PI;
+let bankinh = parseInt(inputbankinh);
+let chuvi = bankinh * 2 * property_value;
+alert(chuvi);
+
+
+
+
+
+inputbankinh = prompt("enter bán kính");
+let bankinh = parseInt(inputbankinh);
+let area = ( bankinh * bankinh )* 2 * Math.PI;
+alert(area);
